f7ae535d0b61b162f3494ae73ec7742dd6d9c43e
nagraj001 <nagk785@gmail.com>
2017-08-20 12:21:29 +0530

    my project setup was successfull

---
+++ shoppingbackend/src/main/java/com/nag/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.nag.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
